2c676ff34f86a6ac2dcfbf71d687a8826bb6601d
ShiruvatiNarasimha <mrnattyboy05@gmail.com>
2025-02-07 22:37:02 +0530

    Updated Dashboard

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,11 +1,46 @@
 "use client";
 
-import { useUser } from "@clerk/nextjs";
+import useProject from "@/hooks/use-project";
+import { ExternalLink, Github } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 const Dashboard = () => {
-  const { user } = useUser();
-  return <div>{user?.firstName}</div>;
+  const { project } = useProject();
+  return (
+    <div>
+      <div className="flex flex-wrap items-center justify-between gap-y-4">
+        <div className="w-fit rounded-md bg-blue-700 bg-primary px-4 py-3">
+          <div className="flex items-center">
+            <Github className="size-5 text-white" />
+            <div className="ml-2">
+              <p className="text-sm font-medium text-white">
+                This project is Linked to{" "}
+                <Link
+                  href={project?.githubUrl ?? ""}
+                  className="inline-flex items-center text-white/80 hover:underline"
+                >
+                  {project?.githubUrl}
+                  <ExternalLink className="ml- size-4" />
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="h-4"></div>
+        <div className="flex items-center gap-4">
+          TeamMembers InviteButton ArchiveButton
+        </div>
+      </div>
+
+      <div className="mt-4">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-5">
+          AskQuestionCard MeetingCard
+        </div>
+      </div>
+      <div className="mt-8">commitLog</div>
+    </div>
+  );
 };
 
 export default Dashboard;
