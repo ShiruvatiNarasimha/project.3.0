96bd4d6de0fc5e752b61166d06dc6cd1fc534093
ShiruvatiNarasimha <mrnattyboy05@gmail.com>
2025-02-07 23:29:54 +0530

    OctoKit node

--- src/lib/github.ts
+++ src/lib/github.ts
@@ -1,1 +1,26 @@
-console.log("Hey There");
+import { Octokit } from "octokit";
+export const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+});
+
+const githubUrl = "https://github.com/docker/genai-stack";
+
+type Response = {
+  commitHash: string;
+  commitMessage: string;
+  commitAuthorName: string;
+  commitAuthorAvatar: string;
+  CommitDate: string;
+};
+
+export const getCommitHashes = async (
+  githubUrl: string,
+): Promise<Response[]> => {
+  const { data } = await octokit.rest.repos.listCommitCommentsForRepo({
+    owner: "docker",
+    repo: "genai-stack",
+  });
+  console.log(data);
+};
+
+getCommitHashes(githubUrl);
