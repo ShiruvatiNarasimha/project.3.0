09b40e61d1c0bec2347aed315c84d9ff3eb4c386
ShiruvatiNarasimha <mrnattyboy05@gmail.com>
2025-02-11 21:25:09 +0530

    Embedding Summary

--- src/lib/github-loader.ts
+++ src/lib/github-loader.ts
@@ -1,4 +1,6 @@
 import { GithubRepoLoader } from "@langchain/community/document_loaders/web/github";
+import { Document } from "@langchain/core/documents";
+import { generateEmbedding, summariseCode } from "./gemini";
 
 export const loadGithubRepo = async (
   githubUrl: string,
@@ -21,6 +23,26 @@ export const loadGithubRepo = async (
   return docs;
 };
 
-console.log(
-  await loadGithubRepo("https://github.com/ShiruvatiNarasimha/AI-Blog"),
-);
+export const indexGithubRepo = async (
+  projectId: string,
+  githubUrl: string,
+  githubToken?: string,
+) => {
+  const docs = await loadGithubRepo(githubUrl, githubToken);
+  const allEmbeddings = await generateEmbeddings(docs);
+};
+
+const generateEmbeddings = async (docs: Document[]) => {
+  return await Promise.all(
+    docs.map(async (doc) => {
+      const summary = await summariseCode(doc);
+      const embedding = await generateEmbedding(summary);
+      return {
+        summary,
+        embedding,
+        sourceCode: JSON.parse(JSON.stringify(doc.pageContent)),
+        fileName: doc.metadata.source,
+      };
+    }),
+  );
+};
