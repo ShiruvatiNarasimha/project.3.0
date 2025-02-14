e801cddd8040a10c280364704da33ec2a40c054f
ShiruvatiNarasimha <mrnattyboy05@gmail.com>
2025-02-14 14:06:01 +0530

    Updated QusetionCard

--- src/app/(protected)/dashboard/ask-question-card.tsx
+++ src/app/(protected)/dashboard/ask-question-card.tsx
@@ -2,19 +2,41 @@
 
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
 import { Textarea } from "@/components/ui/textarea";
 import useProject from "@/hooks/use-project";
+import Image from "next/image";
 import React from "react";
 
 const AskQuestionCard = () => {
   const { project } = useProject();
+  const [open, setOpen] = React.useState(false);
   const [question, setQuestion] = React.useState("");
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    window.alert(question);
+    setOpen(true);
   };
   return (
     <>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>
+              <Image
+                src="/undraw_github.svg"
+                alt="Github-Ai"
+                width={40}
+                height={40}
+              />
+            </DialogTitle>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
       <Card className="relative col-span-2">
         <CardHeader>
           <CardTitle>Ask a question</CardTitle>
